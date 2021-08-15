c2551e667723b052aece45f91a0e3631350a0f57
Greg <gregory.vettorel@student.ehb.be>
2021-08-15 18:42:25 +0200

    delete werkt niet

--- front/script.js
+++ front/script.js
@@ -14,20 +14,6 @@ window.onload = () => {
             // console.log(data[i].name);
            // console.log(data[i].image);
             const htmlString = `
-            <form action="http://127.0.0.1:5500/front/myPortfolio.html" method="DELETE" id="form">
-            <label for="name"></label>
-            <input style="display:none" type="text" name="name" id="name" for="name" value="${data[i].name}">
-            <label for="author"></label>
-            <input style="display:none" type="text" name="author" id="author" for="author" value="${data[i].author}">
-            <label for="Plot"></label>
-            <input style="display:none" type="text" name="Plot" id="Plot" for="Plot" value="${data[i].Plot}">
-            <label for="publishdate"></label>
-            <input style="display:none" type="text" name="publishdate" id="publishdate" for="publishdate" value="${data[i].publishdate}">
-            <label for="image"></label>
-            <input style="display:none" type="text" name="image" id="image" for="image" value="${data[i].image}">
-            <label for="preview"></label>
-            <input style="display:none" type="text" name="preview" id="preview" for="preview" value="${data[i].preview}">
-
             <div class="dataCard">
         <img src="${data[i].image}" id="bookThumbnail">
         <div id="containerBook">
@@ -36,7 +22,6 @@ window.onload = () => {
         <div class="divTitles"><p><b>Description:</b></p><p id="description">${data[i].Plot}</p></div><br>
         <div class="divTitles"><b><p>Published date:</b></p><p id="publishYear">${data[i].publishdate}</p></div><br>
         <input type="submit" id="addtoPortfolio" value="Delete from portfolio">
-        </form>
         <a href="${data[i].preview}" target="_blank"><button id="addtoPortfolio">Preview</button></a>
         </div>
     </div>`;
